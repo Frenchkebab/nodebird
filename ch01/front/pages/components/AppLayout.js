07bdd45b89f3fdc57e713530500e420deda0cf75
frenchkebab <tubowangin@gmail.com>
2022-08-06 10:08:20 +0200

    [#2] antd와 styled-components

--- ch01/front/pages/components/AppLayout.js
+++ ch01/front/pages/components/AppLayout.js
@@ -1,21 +1,27 @@
 import PropTypes from 'prop-types';
 import Link from 'next/link';
+import { Menu } from 'antd';
 
 const AppLayout = ({ children }) => {
   return (
     <div>
-      <div>
-        <Link href="/">
-          <a>NodeBird</a>
-        </Link>
-        <Link href="/profile">
-          <a>Profile</a>
-        </Link>
-        <Link href="/signup">
-          <a>Signup</a>
-        </Link>
-      </div>
-      {children}
+      <Menu>
+        <Menu.Item>
+          <Link href="/">
+            <a>NodeBird</a>
+          </Link>
+        </Menu.Item>
+        <Menu.Item>
+          <Link href="/profile">
+            <a>Profile</a>
+          </Link>
+        </Menu.Item>
+        <Menu.Item>
+          <Link href="/signup">
+            <a>Signup</a>
+          </Link>
+        </Menu.Item>
+      </Menu>
     </div>
   );
 };
---
+++ ch02/front/README.md
@@ -0,0 +1,3 @@
+### antd, styled-components 설치
+
+`npm i antd styled-components @ant-design/icons`
