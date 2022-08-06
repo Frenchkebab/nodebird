a505ebfc36a7fba90de5404242774ef3eef1a5fd
frenchkebab <tubowangin@gmail.com>
2022-08-06 22:29:55 +0200

    [#2] 회원가입 폼 setState 노가다로 만들기

--- ch01/front/pages/signup.js
+++ ch01/front/pages/signup.js
@@ -1,14 +1,67 @@
+import { useCallback, useState } from 'react';
 import AppLayout from '../components/AppLayout';
 import Head from 'next/head';
+import { Form, Input } from 'antd';
 
 const Signup = () => {
+  const [nickname, setNickname] = useState('');
+  const onChangeNickname = useCallback((e) => {
+    setNickname(e.target.value);
+  });
+
+  const [id, setId] = useState('');
+  const onChangeId = useCallback((e) => {
+    setId(e.target.value);
+  }, []);
+
+  const [password, setPassword] = useState('');
+  const onChangePassword = useCallback((e) => {
+    setPassword(e.target.value);
+  }, []);
+
+  const [passwordCheck, setPasswordCheck] = useState('');
+  const onChangePasswordCheck = useCallback((e) => {
+    setPasswordCheck(e.target.value);
+  }, []);
+
+  const onSubmit = useCallback(() => {
+    // 내부적으로 e.preventDefault()가 자동으로 됨
+  }, []);
+
   return (
     <>
       <Head>
         <title>회원가입 | NodeBird</title>
       </Head>
       <AppLayout>
-        <div>회원가입 페이지</div>
+        <Form onFinish={onSubmit}>
+          <div>
+            <label htmlFor="user-nickname">닉네임</label>
+            <br />
+            <Input name="user-nickname" value={nickname} required onChange={onChangeNickname} />
+          </div>
+          <div>
+            <label htmlFor="user-id">아이디</label>
+            <br />
+            <Input name="user-id" value={id} required onChange={onChangeId} />
+          </div>
+          <div>
+            <label htmlFor="user-password">비밀번호</label>
+            <br />
+            <Input name="user-password" type="password" value={password} required onChange={onChangePassword} />
+          </div>
+          <div>
+            <label htmlFor="user-password-check">비밀번호 체크</label>
+            <br />
+            <Input
+              name="user-password-check"
+              type="password"
+              value={passwordCheck}
+              required
+              onChange={onChangePasswordCheck}
+            />
+          </div>
+        </Form>
       </AppLayout>
     </>
   );
